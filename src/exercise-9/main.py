7a98d073922250cc78b920a127905bff8b1f53a1
Kevin Tong <kcr1230@hotmail.com>
2017-11-07 00:00:52 -0800

    Auto stash before merge of "master" and "origin/master"

--- src/exercise-9/main.py
+++ src/exercise-9/main.py
@@ -8,26 +8,28 @@ def convert_f_to_c(c):
 def temperature_conversion(minimum, maximum):
     while True:
         print("Please input a valid minimum temperature value between "
-              + str(minimum) + "C and " + str(maximum) + "C:")
+              + str(minimum) + "C° and " + str(maximum) + "C°:")
         min_temp = input()
         print("Please input a valid maximum temperature value between "
-              + str(minimum) + "C and " + str(maximum) + "C:")
+              + str(minimum) + "C° and " + str(maximum) + "C°:")
         max_temp = input()
         try:
             min_temp = int(min_temp)
             max_temp = int(max_temp)
         except ValueError:
             print("Please input a valid temperature value between "
-                  + str(minimum) + "C and " + str(maximum) + "C:")
+                  + str(minimum) + "C° and " + str(maximum) + "C°!")
+            continue
         if (float(minimum) <= float(min_temp) <= float(maximum)) \
                 | (float(minimum) <= float(max_temp) <= float(maximum)):
             break
         else:
             print("Invalid range!")
 
+    print("  C°        F°")
     for i in range(min_temp, max_temp + 1):
         f = float(convert_f_to_c(i))
-        print("{:.2f}".format(i) + "C => " + "{:.2f}".format(f) + "F")
+        print("{:.2f}".format(i) + "C° => " + "{:.2f}".format(f) + "F°")
 
 
 def main():
