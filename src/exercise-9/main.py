f37c5c3cb808749a377bd6e3f06499feb706a72b
Kevin Tong <kcr1230@hotmail.com>
2017-11-06 19:14:19 -0800

    asdf

---
+++ src/exercise-9/main.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def convert_f_to_c(c):
+    return c * (9.0 / 5.0) + 32
+
+
+def temperature_conversion(minimum, maximum):
+    while True:
+        print("Please input a valid minimum temperature value between "
+              + str(minimum) + "C and " + str(maximum) + "C:")
+        min_temp = input()
+        print("Please input a valid maximum temperature value between "
+              + str(minimum) + "C and " + str(maximum) + "C:")
+        max_temp = input()
+        try:
+            min_temp = int(min_temp)
+            max_temp = int(max_temp)
+        except ValueError:
+            print("Please input a valid temperature value between "
+                  + str(minimum) + "C and " + str(maximum) + "C:")
+        if (float(minimum) <= float(min_temp) <= float(maximum)) \
+                | (float(minimum) <= float(max_temp) <= float(maximum)):
+            break
+        else:
+            print("Invalid range!")
+
+    for i in range(min_temp, max_temp + 1):
+        f = float(convert_f_to_c(i))
+        print("{:.2f}".format(i) + "C => " + "{:.2f}".format(f) + "F")
+
+
+def main():
+    temperature_conversion(-273.00, 5000.00)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
--- src/test/test.py
+++ src/test/test.py
@@ -1,1 +1,37 @@
-print("hello")
+import sys
+
+
+def main():
+    print("hello")
+    print(fib(4))
+
+    a = BankAccount
+    a.deposit(a, 500)
+    a.get_balance()
+
+
+class BankAccount:
+    def __init__(self):
+        self._balance = 0
+
+    def withdraw(self, amount):
+        self._balance -= amount
+
+    def deposit(self, amount):
+        self._balance += amount
+
+    def get_balance(self):
+        return self._balance
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
